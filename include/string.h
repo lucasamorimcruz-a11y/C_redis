486f68c04b7aa72fbee1c566809177cb4919caa6
Lucas Amorim <lucas.amorim.cruz@gmail.com>
2026-03-23 09:42:38 -0300

    Update string.h

--- include/string.h
+++ include/string.h
@@ -1,14 +1,1 @@
-/* Implementação de strings dinâmicas do REDIS */
-#ifndef STRING_H
-#define STRING_H
-#include <my_ints.h>
 
-
-typedef struct dynamic_strings {
-    int64 len;  
-    char *string;  
-} dynamic_strings;
-
-void insert();
-
-#endif 
