2010166568a6c19fade2be8ad88273ccf55fd205
lucas <lucas.amorim.cruz@gmail.com>
2026-05-04 11:15:06 -0300

    update

--- include/sds.h
+++ include/sds.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
-typedef char *sds;
+typedef char* sds;
 typedef struct sds_header
 {
     uint64 len;
@@ -13,13 +13,13 @@ typedef struct sds_header
     char buf[];
 } sds_header;
 
-sds new_sds(uint64 cap);
-sds sds_from (const void *data, uint64 len);
+sds sdsnew (const char *str);
+sds sds_from (const char* str, uint64 len);
+struct sds_header *get_header (sds string);
 sds make_room_for (sds source, uint64 needed);
 void sds_free (sds string);
 void set_sds_len (sds string);
 uint64 sds_len(const sds string);
-int sds_reserve (sds string, uint64 needed);
 char sds_data (const sds string);
 void sds_append(sds string, sds to_be_appended);
 char get_at (const sds string, uint64 index);
@@ -29,5 +29,7 @@ void sds_to_upper (sds string);
 int sds_cmp(const sds string1, const sds string2);
 void sds_clear (sds string);
 sds sds_copy (sds dest, const sds src);
+sds cat (sds s, const char *t);
+
 
 #endif 
--- src/sds.c
+++ src/sds.c
@@ -1,11 +1,43 @@
 #include "sds.h"
 
+sds sdsnew (const char *str){
+    return sds_from (str, strlen(str));
+}
+sds sds_from (const char* str, uint64 len){
+    struct sds_header *sh = malloc (sizeof (struct sds_header) +  len + 1);
+    if (sh == NULL) {
+        return NULL;
+    }
+    sh->len = len;
+    sh->free = 0;
 
-int main(void)
-{
-    test();
+    sds string = sh->buf;
+    memcpy(string, str, len);
+    string[len] = '\0';
+    return string;
+}
+struct sds_header *get_header (sds string){
+    return (struct sds_header *) ((char *) string - sizeof(struct sds_header));
+}
+sds make_room_for (sds source, uint64 needed){
 }
 
-void test()
-{
+void sds_free (sds string){
+
+}
+void set_sds_len (sds string){
+
+}
+uint64 sds_len (const sds string){
+    struct sds_header *ptr = get_header(string);
+    return ptr->len;
+}
+
+int main (){ 
+    printf("Teste\n");
+    sds string = sdsnew ("Hello there!");
+    int len = sds_len(string);
+    printf("%d\n", len);
+    strlen (string);
+    return 0;
 }
